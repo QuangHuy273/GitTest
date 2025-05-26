368d9b873647e312db029165486e4e810f3ba2f6
QuangHuy <dontcomeback0372@gmail.com>
2025-05-26 22:16:04 +0700

    xoa het code

--- ExamDistribution/Interfaces/IStaffMajorFacilityService.cs
+++ ExamDistribution/Interfaces/IStaffMajorFacilityService.cs
@@ -1,15 +1,15 @@
-﻿using ExamDistribution.Models;
-using ExamDistribution.ViewModels;
+﻿//using ExamDistribution.Models;
+//using ExamDistribution.ViewModels;
 
-namespace ExamDistribution.Services.Interfaces
-{
-    public interface IStaffMajorFacilityService
-    {
-        Task<List<StaffMajorFacilityViewModel>> GetByStaffIdAsync(string staffId);
-        Task<bool> CheckDuplicateAsync(string staffId, string facilityId);
-        Task AddAsync(string staffId, string majorFacilityId);
-        Task DeleteAsync(string id);
-    }
+//namespace ExamDistribution.Services.Interfaces
+//{
+//    public interface IStaffMajorFacilityService
+//    {
+//        Task<List<StaffMajorFacilityViewModel>> GetByStaffIdAsync(string staffId);
+//        Task<bool> CheckDuplicateAsync(string staffId, string facilityId);
+//        Task AddAsync(string staffId, string majorFacilityId);
+//        Task DeleteAsync(string id);
+//    }
 
-}
+//}
 
